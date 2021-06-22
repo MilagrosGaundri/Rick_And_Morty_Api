11e921c9ee261fd604d2e2039872d01bfdb007cc
MilagrosGaundri <miligaundri10mail.com>
2021-06-22 11:35:01 -0300

    Instalando Dependencias de Desarrollo en NPM

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,21 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'js/bundle.[contenthast].js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPack: ""
+    },
+    mode: "production",
+    module:{
+        rules:[
+            {
+                test: /\.(js|jsx)$/,
+                use: 'babel-loader',
+                exclude: /node_modules/,
+                resolve:{
+                    extensions: ['.js', '.jsx'],
+                },
+            },
+            
+        ]
     }
 }
